8c420a096c2db17a47f2ba1f4a38810046d53694
Monty <moya73pochta010@gmail.com>
2025-09-12 15:24:07 +0300

    Создал классы трубы и КС, где описаны их свойства. Объявил соответствующие переменные. Добавил ТЗ/отслеживание прогресса для себя в README

---
+++ lr1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+struct Pipe
+{
+    string name;
+    float length;
+    int diametr;
+    bool status;
+};
+
+struct CS
+{
+    int number_buildings;
+    int number_online;
+    string class_cs;
+};
+
+int main()
+{
+    Pipe truba;
+    CS comp_station;
+    return 1;
+}
